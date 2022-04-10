b2eb69b01a56b010e14aa4177f61c662d609695f
Arjunpopzz <92503793+Arjunpopzz@users.noreply.github.com>
2022-04-11 00:02:28 +0530

    Add files via upload

--- lab1.c
+++ lab1.c
@@ -2,35 +2,37 @@
 int main()
 {
     int a,b,c;
+    for(;;)
+    {
     printf("ENTER THE VALUES FOR THE THREE SIDES OF AN TRIANGLE\n");
     scanf("%d%d%d",&a,&b,&c);
     while(a!=0 && b!=0 && c!=0)
     {
         if (a>=10 || b>=10 || c>=10 )
         {
-           printf("THE VALUE IS OUT OF RANGE,TRAINGLE CANNOT BE FORMED");
+           printf("THE VALUE IS OUT OF RANGE,TRAINGLE CANNOT BE FORMED\n");
            break;
         }
         if(a==b && b==a && c==a)
         {
-           printf("THE TRIANGLE IS EQUILATERAL:");
+           printf("THE TRIANGLE IS EQUILATERAL:\n");
            break;
         }
         else if(a!=b && b!=a && c!=b)
          {
-            printf("THE TRIANGLE IS SCALENE:");
+            printf("THE TRIANGLE IS SCALENE:\n");
             break;
          }
         else
         {
-            printf("THE TRIANGLE IS ISOSCELES");
+            printf("THE TRIANGLE IS ISOSCELES\n");
             break;
         }
    
     }
     if(a==0 || b==0 || c==0) 
-        printf("SIDE OF A TRIANGLE CANNOT BE ZERO");
+        printf("SIDE OF A TRIANGLE CANNOT BE ZERO\n");
     
-
+    }
     return 0;
 }
